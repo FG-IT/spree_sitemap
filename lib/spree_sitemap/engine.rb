6de3452984b2ab0e378f8d2c95483dc44681ad12
Neal Wong <neal.wkacc@gmail.com>
2022-04-08 15:46:54 -0400

    Temp code.

--- lib/spree_sitemap/engine.rb
+++ lib/spree_sitemap/engine.rb
@@ -7,7 +7,7 @@ module SpreeSitemap
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Spree::Product.class_eval do
+      ::Spree::Product.class_eval do
         def self.last_updated
           last_update = order('spree_products.updated_at DESC').first
           last_update.try(:updated_at)
